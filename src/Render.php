04aab78a0dd34ec5efef11f7104da25e0fbbefe7
L-yangshuiping <yangshuiping@icloudshield.com>
2022-05-16 10:26:40 +0800

    change a lot ,support for multi-examples

--- src/Render.php
+++ src/Render.php
@@ -26,6 +26,21 @@ class Render
     protected const TEMPLATE_POST = '/templates/template_post.txt';
     protected const TEMPLATE_CEPT = '/templates/template_cept.txt';
     protected const REQUEST_PARAM_MODE = 'formdata';
+    // postman里面可能存在全局host变量，使用下面或者替换
+    const REQUEST_URL = 'https://tool.oschina.net/regex';
+    const RESPONSE_CHACK = 'response_check';
+    // 返回的状态码字段名称
+    const RESPONSE_CODE = 'flag';
+    // 返回的数据字段名称
+    const RESPONSE_DATA = 'msg';
+    // 默认的json返回结果检查规则
+    const DEFAULT_RULES = [
+        'code' => 'integer:=200',
+        'message' => 'string',
+        'data' => 'array',
+    ];
+    // 不解析的字段
+    const NOT_PARSE = ['info', 'list'];
 
     /**
      * @var
@@ -41,13 +56,14 @@ class Render
     public function renderCept($item, array $function_codes): string
     {
         static $var = 0;
+        $slices = explode('/', $item->name);
         if ($var < 1) {
             $this->template = file_get_contents(__DIR__ . self::TEMPLATE_CEPT);
+            $this->renderParam(self::PARAM_CEPT_CEPTNAME, 'Api');
+            $var++;
         }
-        $this->renderParam(self::TEMPLATE_POST, $item->name);
         $this->renderParam(self::PARAM_CEPT_METHODS, implode('', $function_codes));
 
-        $var++;
         return $this->template;
     }
 
@@ -57,42 +73,115 @@ class Render
      * @return string
      * @throws \Exception
      */
-    public function renderFunction($request): string
+    public function renderFunction($request_item): string
     {
-        if (empty($request->request->method)) {
-            throw new \Exception(sprintf('Empty %s method type', $request->request->method));
+        if (empty($request_item->method)) {
+            throw new \Exception(sprintf('Empty %s method type', $request_item->method));
         }
-        if (empty($this->getMethodTemplate($request->request->method))) {
-            throw new \Exception(sprintf('Dont have %s type template', $request->request->method));
+        if (empty($this->getMethodTemplate($request_item->method))) {
+            throw new \Exception(sprintf('Dont have %s type template', $request_item->method));
         }
 
-        $this->template = file_get_contents($this->getMethodTemplate($request->request->method ?? ''));
-        $this->renderParam(self::PARAM_NAME, $request->name ?? '');
-        $this->renderParam(self::PARAM_DESCRIPTION, $request->description ?? '');
-        $this->renderParam(self::PARAM_URL, $request->request->url->raw ?? '');
+        $this->template = file_get_contents($this->getMethodTemplate($request_item->method ?? ''));
+        if (strlen($request_item->name) > 20) {
+            $request_item->name = parse_url($request_item->name, PHP_URL_PATH);
+        }
+        $slices = explode('/', $request_item->name);
 
-        // need to handle some kind of params
-        switch ($request->request->body->mode) {
+        $this->renderParam(self::PARAM_NAME, $slices[3] ?? $slices[1] ?? $slices[0]);
+        $this->renderParam(self::PARAM_DESCRIPTION, $request_item->description ?? 'Test ' . $request_item->name);
+        $this->renderParam(self::PARAM_URL, $request_item->url->raw ?? '');
+        // 将保存的示例解析出来做判断，如果不存在示例，那就使用默认规则
+        if (empty($request_item->response_body)) {
+            $this->renderParam(self::RESPONSE_CHACK, self::DEFAULT_RULES);
+        } else {
+            $response_body = json_decode($request_item->response_body, true);
+            foreach ($response_body as $key => $value) {
+                if ($key !== self::RESPONSE_DATA) {
+                    $rules[$key] = gettype($value);
+                }
+                if ($key === self::RESPONSE_CODE) {
+                    $rules[$key] = gettype($value) . ':=' . $value;
+                }
+                if (is_array($value)) {
+                    $rules[$key] = $this->getValueType($value);
+                }
+            }
+            // 检查返回结果里面的字段规则，并替换
+            $this->renderParam(self::RESPONSE_CHACK, var_export($rules, true) ?? '');
+        }
+
+        // 如果没有参数，就直接替换为空数组
+        if (empty($request_item->body)) {
+            $this->renderParam(self::PARAM_PARAMS, var_export([], true));
+            return $this->template;
+        }
+
+        switch ($request_item->body->mode) {
             case 'raw':
-                if (!empty($request->request->body->raw)) {
-                    $this->renderParam(self::PARAM_PARAMS, (array)$request->request->body->raw);
+                if (!empty($request_item->body->raw)) {
+                    $this->renderParam(self::PARAM_PARAMS, $this->changeParams(json_decode($request_item->body->raw, true)));
                 }
                 break;
             case 'formdata':
             case 'urlencoded':
-                if (!$request->request->body->formdata && !$request->request->body->urlencoded) {
+                // 如果没有param，直接使用空数组代替
+                if (empty($request_item->body->formdata) && empty($request_item->body->urlencoded)) {
                     $this->renderParam(self::PARAM_PARAMS, []);
                     break;
                 }
-                $formdata = $request->request->body->formdata ?? $request->request->body->urlencoded;
+                $formdata = $request_item->body->formdata ?? $request_item->body->urlencoded;
                 $params = [];
                 foreach ($formdata as $formdatum) {
                     $params[$formdatum->key] = $formdatum->value;
                 }
-                $this->renderParam(self::PARAM_PARAMS, $params);
+                $this->renderParam(self::PARAM_PARAMS, $this->changeParams($params));
+                break;
+            default:
+                $this->renderParam(self::PARAM_PARAMS, var_export([], true));
                 break;
         }
         return $this->template;
+    }
+
+    /**
+     * 递归获取值类型，并保持结构不变
+     * @author Bruce 2022/5/13
+     * @param $arr
+     * @return mixed
+     */
+    public function getValueType($arr)
+    {
+        foreach ($arr as $k => $v) {
+            if (is_array($arr[$k])) {
+                if (in_array($k, self::NOT_PARSE)) {
+                    $arr[$k] = 'array';
+                } else {
+                    $arr[$k] = $this->getValueType($arr[$k]);
+                }
+            } else {
+                $gettype = gettype($arr[$k]);
+                $arr[$k] = $gettype === 'double' ? 'float' : $gettype;
+            }
+        }
+        return $arr;
+    }
+
+    /**
+     * changeParams
+     * @author Bruce 2022/5/12
+     * @param array $params
+     * @return array
+     */
+    public function changeParams(array $params)
+    {
+        foreach ($params as $key => $param) {
+            // 如果是数字类型的字符串，且第一个数字不是0，那么就需要转成数字参数
+            if (is_string($param) && is_numeric($param) && strpos($param, '0') !== 0) {
+                $params[$key] = $param + 0;
+            }
+        }
+        return $params;
     }
 
     /**
--- src/StrategyPostman.php
+++ src/StrategyPostman.php
@@ -34,10 +34,35 @@ class StrategyPostman extends StrategyBase implements Generateable
     {
         parent::generate($data);
         /** @todo replace StdClass by Structs */
+        $result = [];
         foreach ($this->data->item as $item) {
-            echo PHP_EOL . 'List: ' . $item->name;
-            $this->processCollectionFolder($item);
+            // 如果没有example
+            if (empty($item->response)) {
+                echo PHP_EOL . 'List: ' . $item->name;
+                $item->request->name = $item->name;
+                $result[] = $this->processCollectionFolder($item->request);
+            } else {
+                // 如果有多个response，分别做匹配
+                $responses = $item->response;
+                // 把每个example都加入到方法
+                foreach ($responses as $response) {
+                    echo PHP_EOL . 'List: ' . $response->name;
+                    $response->originalRequest->name = $response->name;
+                    $response->originalRequest->response_body = $response->body;
+                    $result[] = $this->processCollectionFolder($response->originalRequest);
+                }
+            }
         }
+        // 全部内容都写到这个文件中
+        $filename = $this->directory . 'ApiCest.php';
+
+        // 匹配修改{{host}}
+        $pattern = '/[\{].*[\}]/';
+        $replacement = Render::REQUEST_URL;
+        $result = preg_replace($pattern, $replacement, implode(PHP_EOL, $result) . '}');
+        // 把所有结果输入到文件中
+        echo PHP_EOL . 'Result: ' . file_put_contents($filename, $result);
+        echo PHP_EOL . 'END';
 
         return true;
     }
@@ -47,16 +72,13 @@ class StrategyPostman extends StrategyBase implements Generateable
      * @param object $item
      * @throws \Exception
      */
-    protected function processCollectionFolder($item): void
+    protected function processCollectionFolder($item): string
     {
         $function_codes = [];
         echo PHP_EOL . 'Request: ' . $item->name;
         $function_codes[] = $this->render->renderFunction($item);
 
-        $result = $this->render->renderCept($item, $function_codes);
-        // ddd($function_codes,$result);
-        echo PHP_EOL . 'Result: ' . file_put_contents($this->directory . 'Test.php', $result, FILE_APPEND);
-        echo PHP_EOL . 'END';
+        return $this->render->renderCept($item, $function_codes);
     }
 
 }
